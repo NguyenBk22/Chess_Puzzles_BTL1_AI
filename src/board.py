9987118204320ad1d7d067e2ca55b3d274f2f26e
Lê Nguyễn Gia Hưng <ghung21112004@gmail.com>
2025-01-21 18:13:23 +0700

    update  convert_valid_position, place_piece, remove_piece in board.py

--- src/board.py
+++ src/board.py
@@ -1,10 +1,20 @@
+from pieces import King, Knight, Queen, Pawn, Rook, Bishop
+
 class Board:
     def __init__(self):
+        self.piece = {
+            'K': King,
+            'Q': Queen,
+            'N': Knight,
+            'P': Pawn,
+            'R': Rook,
+            'B': Bishop,
+        }
         self.board = []
         for i in range(8):
             self.board.append([None] * 8) # 8 columns each row
 
-    def place_piece(self, piece, pos):
+    def convert_valid_position(self, pos):
         x = 8 - pos[1]
         y = pos[0]
         if y == 'a':
@@ -25,12 +35,24 @@ class Board:
             y = 7
         else:
             return "This is invalid position"
+        return x, y
+    
+    def place_piece(self, piece, pos):
+        x, y = self.convert_valid_position(pos)
+        pos[0] = x
+        pos[1] = y
         
-        self.board[x, y] = piece
+        self.board[x][y] = piece
+        piece_upper = piece.upper()
+        
+        if piece_upper in self.piece:
+            self.board[x][y] = self.piece[piece_upper](pos)
+        else:
+            return "Invalid Piece"
 
-    def remove_piece(self, position):
-        x, y = position
-        self.board[x, y] = None
+    def remove_piece(self, pos):
+        x, y = self.convert_valid_position(pos)
+        self.board[x][y] = None
 
     def is_valid_move(self, piece, new_position):
         if new_position == None:
